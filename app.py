ef200625a72ce86a2e0e2f43d579c1531bbc430f
vainiovesa <vesa.vainio@helsinki.fi>
2025-06-12 14:51:45 +0300

    Add login and register pages

--- app.py
+++ app.py
@@ -1,10 +1,24 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect, request
 
 app = Flask(__name__)
 
 @app.route("/")
 def index():
     return render_template("index.html")
+
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    if request.method == "POST":
+        return redirect("/")
+
+    return render_template("login.html")
+
+@app.route("/register", methods=["GET", "POST"])
+def register():
+    if request.method == "POST":
+        return redirect("/")
+
+    return render_template("register.html")
 
 if __name__=="__main__":
     app.run(debug=True)
