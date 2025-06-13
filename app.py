66c92a379a84dca77a5faea18e6171ba6626f692
vainiovesa <vesa.vainio@helsinki.fi>
2025-06-13 10:20:49 +0300

    Add making new users

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template, redirect, request
+import sqlite3
 
 app = Flask(__name__)
 
@@ -16,9 +17,24 @@ def login():
 @app.route("/register", methods=["GET", "POST"])
 def register():
     if request.method == "POST":
+        username = request.form["username"]
+        password_1 = request.form["password-1"]
+        password_2 = request.form["password-2"]
+        if password_1 != password_2:
+            return redirect("/register")
+
+        sql = f"INSERT INTO Users (username, password) VALUES ('{username}', '{password_1}')"
+        db_execute(sql)
+
         return redirect("/")
 
     return render_template("register.html")
+
+def db_execute(sql):
+    con = sqlite3.connect("database.db")
+    con.execute(sql)
+    con.commit()
+    con.close()
 
 if __name__=="__main__":
     app.run(debug=True)
